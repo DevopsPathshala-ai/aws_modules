999fb9b15ae14383d9d1a116c4fdb6ae46d971d7
mangeshchauhan <mangesh.225@gmail.com>
2026-02-07 08:18:20 +0530

    updating vpc configuration

--- vpc/main.tf
+++ vpc/main.tf
@@ -9,11 +9,42 @@ resource "aws_vpc" "this" {
 }
 resource "aws_subnet" "this" {
   for_each = var.subnets
-  vpc_id = aws_vpc.this.id
-  cidr_block = each.value.cidr_block
-  availability_zone = each.value.availability_zone
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = each.value.cidr_block
+  availability_zone       = each.value.availability_zone
+  map_public_ip_on_launch = each.value.public
 
   tags = {
     Name = each.key
+    Type = each.value.public ? "public" : "private"
   }
 }
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
+}
+
+
+resource "aws_route_table_association" "public" {
+  for_each = {
+    for k, v in var.subnets : k => v if v.public
+  }
+
+  subnet_id      = aws_subnet.this[each.key].id
+  route_table_id = aws_route_table.public.id
