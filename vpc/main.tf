29f93ab31c697075e39c8f24b4f30f15cb7b4cc8
mangeshchauhan <mangesh.225@gmail.com>
2026-03-07 13:37:57 +0530

    updating the main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -49,3 +49,11 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.this[each.key].id
   route_table_id = aws_route_table.public.id
 }
+resource "aws_route_table_association" "private" {
+  for_each = {
+    for k, v in var.subnets : k => v if v.public
+  }
+
+  subnet_id      = aws_subnet.this[each.key].id
+  route_table_id = aws_route_table.private.id
+}
